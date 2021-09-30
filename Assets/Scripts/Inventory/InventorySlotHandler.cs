33b5eb5a916cbd23c946d793eb67d0814715e214
Jet <j3tmarch@gmail.com>
2021-09-30 10:43:51 +0300

    Мелкая правка диздока. Добавлены поля под быстрые слоты

--- Assets/Scripts/Inventory/InventorySlotHandler.cs
+++ Assets/Scripts/Inventory/InventorySlotHandler.cs
@@ -7,12 +7,16 @@ public class InventorySlotHandler : MonoBehaviour
     [SerializeField]
     private Inventory inventory;
 
+    [SerializeField] private ItemUIHolder slot1;
+    [SerializeField] private ItemUIHolder slot2;
+    [SerializeField] private ItemUIHolder slot3;
+    [SerializeField] private ItemUIHolder slot4;
+
     // Update is called once per frame
     void Update()
     {
         if(Input.GetKeyDown(KeyCode.Alpha1))
         {
-            Debug.Log("1");
         }
     }
 }
